d6de2ca8c23fc44042ac8d91897c01498601097e
Simon Vu <simon_vu1@hotmail.com>
2018-04-14 21:07:21 +0200

    hello

--- maps/map.js
+++ maps/map.js
@@ -2,6 +2,7 @@
 var map;
 var markers = [];
 
+
 firebase.initializeApp({
 	apiKey: "AIzaSyAx67v4mGr7qE4qSkbI0snWMFayDGeI6do",
 	databaseURL: "https://flaskplay.firebaseio.com/"
@@ -46,11 +47,11 @@ function loadDb(key)
 	});
 }
 
-function saveWater(key, locationData, ph, bacteria)
+function saveWater(key, ph, bacteria, leadLevel)
 {
 	firebase.database().ref('data/' + key).set(
 	{
-		location: locationData,
+		leadLevel: leadLevel,
 		ph: ph,
 		bacteria: bacteria
 	});
@@ -68,19 +69,101 @@ function saveLocation(key, value)
 	});
 }
 
-function createQuery(centerPos, radiusKm)
+function createQuery(searchKey, distance)
 {
-	var geoQuery = geoFire.query(
-	{
-		center: centerPos,
-		radius: radiusKm
-	});
+	geoFire.get(searchKey).then(function(location) {
+		if (location === null) {
+			console.log("Provided key is not in GeoFire");
+		}
+		else {
+			console.log("Provided key has a location of " + location);
+			var geoQuery = geoFire.query(
+			{
+				center: location,
+				radius: distance
+			});
+			
+			geoQuery.on("key_entered", function(key, location, distance)
+			{
+				if (key != searchKey)
+				{
+
+					
+					
+					dataRef.child(key).once('value').then(function (snapshot)
+					{
+						printWater(snapshot, location);
+					});
+				}
+				
+			});
+		}
+		}, function(error) {
+			console.log("Error: " + error);
+	});	
+}
+
+function printWater(snapshot, location)
+{
+
+		
+		var key = snapshot.key;
+		var ph = snapshot.child('ph').val();
+		var leadLevel = snapshot.child('leadLevel').val();
+		var bacteria = snapshot.child('bacteria').val();
+		var dirtLevel = snapshot.child('dirtLevel').val();
+		//var location = snapshot.child('location').val();
+		
+		var contentString = getContentString(snapshot);
+		
+		var lng = Number(location[0]);
+		var lat = Number(location[1]);
+		var loc = {lat: lat, lng: lng};
+		
+		if (ph > 6.5 && ph < 8)
+		{
+			
+			addMarker(loc,'green', contentString);
+		}
+		else
+		{
+			
+			addMarker(loc,'red', contentString);
+		}
+		
+		console.log("id: " + key + " pH level: " + ph + " leadLevel: " + leadLevel + " bacteria: " + bacteria);
+}
+
+function getContentString(snapshot)
+{
+	var key = snapshot.key;
+	var ph = snapshot.child('ph').val();
+	var leadLevel = snapshot.child('leadLevel').val();
+	var bacteria = snapshot.child('bacteria').val();
+	var dirtLevel = snapshot.child('dirtLevel').val();
 	
-	geoQuery.on("key_entered", function(key, location, distance)
-	{
-		console.log("Spot: " + key + " found at " + location + " (" + distance + " km away)"); 
-	}
-	);
+	
+	
+	return '<div id="content">'+
+		'<div id="siteNotice">'+
+		'</div>'+
+		'<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+		'<div id="bodyContent">'+
+		'<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+		'sandstone rock formation in the southern part of the '+
+		'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+		'south west of the nearest large town, Alice Springs; 450&#160;km '+
+		'(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+		'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+		'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+		'Aboriginal people of the area. It has many springs, waterholes, '+
+		'rock caves and ancient paintings. Uluru is listed as a World '+
+		'Heritage Site.</p>'+
+		'<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+		'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+		'(last visited June 22, 2009).</p>'+
+		'</div>'+
+		'</div>';
 }
 
 function getAllDataPoints()
@@ -88,7 +171,57 @@ function getAllDataPoints()
 	var datapoints = [];
 }
 
-function addMarker(location, name)
+function addMarker(location, color, content)
 {
 	
+	if (color == 'green')
+	{
+		var marker = new google.maps.Marker(
+		{
+			position: location,
+			map: map,
+			icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+		}
+		);
+	}
+	else
+	{
+		var marker = new google.maps.Marker(
+		{
+			position: location,
+			map: map
+		}
+		);
+	}
+
+	var infoWindow = new google.maps.InfoWindow(
+	{
+		content: content
+	});
+	
+	marker.addListener('click', function()
+	{
+		infoWindow.open(map, marker);
+	});
+	markers.push(marker);
+	//showMarkers();
 }
+
+function deleteMarkers()
+{
+	for (var i = 0; i < markers.length; i++)
+	{
+		markers[i].setMap(null);
+	}
+	markers = [];
+}
+
+function showMarkers()
+{
+	for (var i = 0; i < markers.length; i++)
+	{
+		markers[i].setMap(map);
+	}
+}
+
+
