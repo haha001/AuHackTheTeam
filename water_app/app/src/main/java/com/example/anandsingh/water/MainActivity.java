8c453fe376c79183a0da3f40626109b2cfcc88ff
Simon Vu <simon_vu1@hotmail.com>
2018-04-15 11:01:09 +0200

    Added API

---
+++ water_app/app/src/main/java/com/example/anandsingh/water/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.anandsingh.water;
+
+import android.net.Uri;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        CustomTabsIntent customTabsIntent = builder.build();
+        customTabsIntent.launchUrl(this, Uri.parse("https://simonvu.dk/maps/"));
+
+    }
+}
